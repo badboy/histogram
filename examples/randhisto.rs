0207d7bd7f274ebd5fe1240b13a4897c8f7aa8f0
Jan-Erik Rediger <janerik@fnordig.de>
2018-09-26 12:12:27 +0200

    Generate a random histogram and show it

---
+++ examples/randhisto.rs
@@ -0,0 +1,33 @@
+//! Generate a random histogram and display it.
+
+extern crate histogram;
+extern crate rand;
+
+use std::io::{self, Write};
+use std::process;
+use rand::{thread_rng, Rng};
+
+fn main() {
+    if let Err(e) = try_main() {
+        let mut stderr = io::stderr();
+        let _ = write!(&mut stderr, "error: {}", e);
+        process::exit(1);
+    }
+}
+
+fn try_main() -> io::Result<()> {
+    let mut rng = thread_rng();
+    let min = 0;
+    let max = 500;
+    let count = 50;
+    let mut hist = histogram::Histogram::exponential(min, max, count);
+
+    let sample_count = 1_000_000;
+    for _ in 0..sample_count {
+        let sample = rng.gen_range(min, max+1);
+        hist.add(sample);
+    }
+
+    println!("{}", hist);
+    Ok(())
+}
